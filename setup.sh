a8190f337c58ea7a3453a001f4ab45aa2c331e30
Ubuntu <ubuntu@ip-172-31-20-123.us-west-1.compute.internal>
2013-08-01 04:26:13 +0000

    fixed node installation to allow installation of express

--- setup.sh
+++ setup.sh
@@ -8,20 +8,26 @@ sudo apt-get install -y git-core
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+#source $HOME/.nvm/nvm.sh
+#nvm install v0.10.12
+#nvm use v0.10.12
 
-# Install jshint to allow checking of JS code within emacs
+sudo apt-get update
+sudo apt-get install -y python-software-properties python g++ make
+sudo add-apt-repository -y ppa:chris-lea.node.js
+sudo apt-get update
+sudo apt-get install -y nodejs
+
+# Install express 
+npm install -g express
+
+#Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
 npm install -g jshint
 
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
 sudo apt-get install -y rlwrap
-
-# Install the python-software-properties package to enable installing PPAs
-sudo apt-get install python-software-properties
 
 # Install emacs24
 # https://launchpad.net/~cassou/+archive/emacs
